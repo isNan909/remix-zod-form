42efacebf424421088186bcd1cf55de8e21a1ccb
ishan-me <ishan02016@gmail.com>
2022-05-22 17:05:58 +0545

    added working initial schema validation form

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,56 @@
 import { ActionFunction } from '@remix-run/node';
-import { Form } from '@remix-run/react';
+import { Form, useActionData } from '@remix-run/react';
+import { z } from 'zod';
+
+// const getValidationErrors = (err: any) => {
+//   const validationErrors = {} as any;
+//   console.log(err);
+//   err.inner.forEach((error: any) => {
+//     if (error.path) {
+//       validationErrors[error.path] = error.message;
+//     }
+//   });
+
+//   return validationErrors;
+// };
 
 export const action: ActionFunction = async ({ request }) => {
-  const formData = Object.fromEntries(await request.formData());
-  console.log(formData);
-  return {};
+  const formPayload = Object.fromEntries(await request.formData());
+
+  const validationSchema = z
+    .object({
+      name: z.string().min(3),
+      email: z.string().email(),
+      confirmEmail: z.string().email(),
+      expertise: z.enum([
+        'Product Designer',
+        'Frontend Developer',
+        'Backend Developer',
+        'Fullstack Developer',
+      ]),
+      url: z.string().url().optional(),
+      availability: z.enum(['Full-time', 'Part-time', 'Contract', 'Freelance']),
+      description: z.string().nullable(),
+    })
+    .refine((data) => data.email === data.confirmEmail, {
+      message: 'Email and confirmEmail should be same email',
+      path: ['confirmEmail'],
+    });
+
+  try {
+    const validatedSchema = validationSchema.parse(formPayload);
+    console.log('Form data is valid for submission:', validatedSchema); //API call can be made here
+  } catch (error) {
+    return {
+      formPayload,
+      error,
+    };
+  }
+  return {} as any;
 };
 
 export default function Index() {
+  const actionData = useActionData();
   return (
     <div>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -27,15 +70,83 @@ export default function Index() {
                   Full name
                 </label>
                 <input
-                  id="full name"
                   name="name"
                   type="text"
-                  autoComplete="name"
-                  required
+                  defaultValue={actionData?.formPayload?.name}
+                  key={actionData?.formPayload?.name}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder=""
+                />
+                <span className="text-sm text-red-500">
+                  {actionData?.error?.issues[0]?.message}
+                </span>
+              </div>
+            </div>
+
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-6">
+                <label
+                  htmlFor="Email"
+                  className="block text-sm font-medium text-gray-700 pb-2"
+                >
+                  Email
+                </label>
+                <input
+                  name="email"
+                  type="text"
+                  defaultValue={actionData?.formPayload?.email}
+                  key={actionData?.formPayload?.email}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder=""
+                />
+                <span className="text-sm text-red-500">
+                  {actionData?.error?.issues[1]?.message}
+                </span>
+              </div>
+            </div>
+
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-6">
+                <label
+                  htmlFor="omfirm Email"
+                  className="block text-sm font-medium text-gray-700 pb-2"
+                >
+                  Comfirm Email
+                </label>
+                <input
+                  name="confirmEmail"
+                  type="email"
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder=""
                 />
               </div>
+              <span className="text-sm text-red-500">
+                {actionData?.error?.issues[2]?.message}
+              </span>
+            </div>
+
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-6">
+                <label
+                  htmlFor="Expertise"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Expertise
+                </label>
+                <select
+                  name="expertise"
+                  className="mt-1 block w-full py-2 px-4 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                >
+                  <option></option>
+                  <option>Product Designer</option>
+                  <option>Frontend Developer</option>
+                  <option>Backend Developer</option>
+                  <option>Fullstack Developer</option>
+                </select>
+              </div>
+              <span className="text-sm text-red-500">
+                {actionData?.error?.issues[3]?.message}
+              </span>
             </div>
 
             <div className="rounded-md shadow-sm -space-y-px">
@@ -44,18 +155,61 @@ export default function Index() {
                   htmlFor="company-website"
                   className="block text-sm font-medium text-gray-700 pb-2"
                 >
-                  Email
+                  Github URL
                 </label>
                 <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
+                  name="url"
+                  type="text"
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder=""
                 />
               </div>
+              <span className="text-sm text-red-500">
+                {actionData?.error?.issues[4]?.message}
+              </span>
+            </div>
+
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-6">
+                <label
+                  htmlFor="company-website"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Currently Available
+                </label>
+                <select
+                  name="availability"
+                  className="mt-1 block w-full py-2 px-4 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                >
+                  <option></option>
+                  <option>Full-time</option>
+                  <option>Part-time</option>
+                  <option>Contract</option>
+                  <option>Freelance</option>
+                </select>
+              </div>
+              <span className="text-sm text-red-500">
+                {actionData?.error?.issues[5]?.message}
+              </span>
+            </div>
+
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-6">
+                <label
+                  htmlFor="company-website"
+                  className="block text-sm font-medium text-gray-700 pb-2"
+                >
+                  Description
+                </label>
+                <textarea
+                  name="description"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder=""
+                />
+              </div>
+              <span className="text-sm text-red-500">
+                {actionData?.error?.issues[6]?.message}
+              </span>
             </div>
 
             <div>
