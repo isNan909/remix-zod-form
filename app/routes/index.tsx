9f6ec747880bdfcf3492dc6a201c2bc7f6cfe9ce
ishan-me <ishan02016@gmail.com>
2022-05-22 15:25:03 +0545

    added base form

---
+++ app/routes/index.tsx
@@ -0,0 +1,66 @@
+export default function Index() {
+  return (
+    <div>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-lg w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Remix Form Validation with Zod
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" value="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-6">
+                <label
+                  htmlFor="company-website"
+                  className="block text-sm font-medium text-gray-700 pb-2"
+                >
+                  Full name
+                </label>
+                <input
+                  id="full name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder=""
+                />
+              </div>
+            </div>
+
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-6">
+                <label
+                  htmlFor="company-website"
+                  className="block text-sm font-medium text-gray-700 pb-2"
+                >
+                  Email
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder=""
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
